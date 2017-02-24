87b9728a96ff0e2f23cdc80e24d6e68bca36f16a
Hugo Kueny <nessotrin@hotmail.fr>
2017-02-24 03:19:04 +0100

    Meilleurs exemples

--- DataServer/main.cpp
+++ DataServer/main.cpp
@@ -12,6 +12,10 @@
 
 #include "Logger.h"
 
+#include <cstdlib>
+
+#include <time.h>
+
 /* Fonction d'entrée du serveur */
 int main(int argc, char **argv)
 {
@@ -38,12 +42,17 @@ int main(int argc, char **argv)
 	Logger::log("Setuping database ...\n");
 
 	/* TESTS */
+	/* initialize random seed: */
+	srand (time(NULL));
 	Med * testMed1 = new Med("Doliprane");
 	Med * testMed2 = new Med("Dafalgan");
 	Med * testMed3 = new Med("Corde+Tabouret");
 	testMed1->setImg("icon_medication1.png");
+	testMed1->setCurrentCount(rand()%10);
 	testMed2->setImg("icon_medication2.png");
+	testMed2->setCurrentCount(rand()%10);
 	testMed3->setImg("icon_medication3.png");
+	testMed3->setCurrentCount(rand()%10);
 	medHandler.addMed(testMed1);
 	medHandler.addMed(testMed2);
 	medHandler.addMed(testMed3);
@@ -55,6 +64,7 @@ int main(int argc, char **argv)
 		sprintf(name, "Medicament %d",i);
 		Med * tmpMed = new Med(name);
 		tmpMed->setImg("https://placeholdit.imgix.net/~text?txtsize=28&bg=0099ff&txtclr=ffffff&txt=300%C3%97300&w=300&h=300&fm=png");
+		tmpMed->setCurrentCount(rand()%10);
 		medHandler.addMed(tmpMed);
 	}
 	
